9f83e84c41533de4783455d93566e32ad05575ba
dipes <Dipesh>
2016-08-21 17:22:25 -0700

    First Commit

---
+++ CircleProgram/Circle.java
@@ -0,0 +1,17 @@
+
+public class Circle {
+	
+	Circle(double radius){
+		
+		double Area = radius*radius*3.14;
+
+	}
+	
+	
+	public static void main(String args[]){
+		
+		Circle myCircle = new Circle(2.5);
+		System.out.println(myCircle);
+		
+}
+}
